7b9bd01b48d16df0025ee8747f6ee0e75903e92c
Prokop <dp260296pvm@gmail.com>
2019-01-08 20:13:09 +0200

    Initial commit

---
+++ src/main/java/com/testptoject/budgetsocialmedia/Application.java
@@ -0,0 +1,13 @@
+package com.testptoject.budgetsocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*Класс для запуска Спринг приложения*/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
