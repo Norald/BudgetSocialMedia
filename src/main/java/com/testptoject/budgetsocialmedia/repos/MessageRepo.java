6b98815f8645c9e8ccc44557d943e4bc3c3339d5
Prokop <dp260296pvm@gmail.com>
2019-01-11 22:49:30 +0200

    -Добавлена поддержка базы банных Spring Boot JPA(PostgreSQL) -ДОбавлена возможность добавления сообщения и поиск сообщения по тэгу -Переработан Mustache

---
+++ src/main/java/com/testptoject/budgetsocialmedia/repos/MessageRepo.java
@@ -0,0 +1,13 @@
+package com.testptoject.budgetsocialmedia.repos;
+
+
+import com.testptoject.budgetsocialmedia.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
